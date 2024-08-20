434ca754d0346057f54e8b4816e2125e014c5177
Dave Jones <davetjones1992@gmail.com>
2024-08-20 19:30:49 +0100

    Adds sphere type with hittable trait

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,22 +1,23 @@
 use crate::{
     hittable::{HitRecord, Hittable},
+    interval::Interval,
     ray::Ray,
     vec3::Point3,
 };
 
-struct Sphere {
+pub struct Sphere {
     center: Point3,
     radius: f64,
 }
 
 impl Sphere {
-   pub fn new(center: Point3, radius: f64) {
-       Self {center, radius}
-   } 
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
 }
 
 impl Hittable for Sphere {
-    fn hit(&self, r: Ray, ray_tmin: f64, ray_tmax: f64, mut rec: HitRecord) -> bool {
+    fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
         let oc = &self.center - &r.origin();
         let a = r.direction().length_squared();
         let h = r.direction().dot(&oc);
@@ -31,16 +32,18 @@ impl Hittable for Sphere {
         // Find the nearest root that lies in the acceptable range.
         let mut root = (h - sqrtd) / a;
 
-        if root <= ray_tmin || ray_tmax <= root {
+        if !ray_t.surrounds(root) {
             root = (h + sqrtd) / a;
-            if root <= ray_tmin || ray_tmax <= root {
+            if !ray_t.surrounds(root) {
                 return false;
             }
         }
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - self.center) / self.radius;
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, outward_normal);
 
         true
     }
+}
